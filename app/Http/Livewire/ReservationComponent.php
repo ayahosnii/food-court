7b2070b30eb85c4751d4feecce95a45ccc4bc8f4
Aya Hosny <96696906+ayahosnii@users.noreply.github.com>
2023-09-16 23:35:39 +0300

    reservation

--- app/Http/Livewire/ReservationComponent.php
+++ app/Http/Livewire/ReservationComponent.php
@@ -2,62 +2,236 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\admin\WorkingHour;
 use App\Models\providers\Provider;
+use App\Models\providers\Table;
+use App\Models\Reservation;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Livewire\Component;
+use Stripe\PaymentIntent;
+use Stripe\Stripe;
+
 
 class ReservationComponent extends Component
-{
-    public $step = 1;
-    public $user;
-    public $name;
-    public $email;
-    public $mobile;
-    public $provider_id;
-    public $res_date;
-    public $guest_number;
-    public $providers;
-    public $availableTables;
-
-
-    public function mount()
     {
-        $this-> providers = Provider::where('accountactivated', '1')->get();
-    }
-    public function nextStep()
-    {
-//        $this->validateStepOne(); // Implement validation rules for step one fields
+        public $step = 1;
+        public $user;
+        public $name;
+        public $email;
+        public $mobile;
+        public $provider_id;
+        public $table_id;
+        public $res_date;
+        public $guest_number;
+        public $providers;
+        public $availableTables;
+        public $availableTableIds = [];
 
-        if ($this->step == 1) {
-            $this->step = 2;
-        }
-    }
+        public $res_time;
 
-    public function previousStep()
-    {
-        $this->step = 1;
-    }
-
-//    private function validateStepOne()
-//    {
-//        $this->validate([
-//            'name' => 'required|string',
-//            'email' => 'required|email',
-//            'mobile' => 'required|string',
-//            'provider_id' => 'required|exists:providers,id',
-//            'res_date' => 'required|date',
-//            'guest_number' => 'required|integer',
-//        ]);
-//    }
-
-    public function render()
-    {
-
-        if ($this->step == 2) {
-            // Fetch available tables for the selected provider and reservation date
+        public function mount()
+        {
+            $this->providers = Provider::where('accountactivated', '1')->get();
         }
 
+        public function nextStep()
+        {
+            if ($this->step == 1) {
+                $this->validateStepOne();
+                $isAvailable = $this->checkTableAvailability();
+
+                if (!$isAvailable) {
+                    notify()->error('There are no available tables for ' . $this->guest_number . ' person on the selected date and time.');
+                    return;
+                }
+
+                $tables = Table::whereIn('id', $isAvailable)->get();
+
+                $this->availableTableIds = $tables;
+                $this->step = 2;
+            }
+        }
+
+        public function submitPayment()
+        {
+            // Set your Stripe API key
+            Stripe::setApiKey(config('services.stripe.secret'));
+
+            // Create a PaymentIntent
+            $paymentIntent = PaymentIntent::create([
+                'amount' => 1000,
+                'currency' => 'usd',
+                'payment_method_types' => ['card'],
+            ]);
+
+            $this->emit('paymentIntentCreated', $paymentIntent->client_secret);
+        }
+        public function storeData()
+        {
+            try {
+                $this->validate([
+                    'name' => 'required|string',
+                    'email' => 'required|email',
+                    'mobile' => 'required|string',
+                    'res_date' => 'required|date',
+                    'res_time' => ['required'],
+                    'table_id' => 'required|exists:tables,id',
+                    'guest_number' => 'required|integer',
+                    'provider_id' => 'required|exists:providers,id',
+                ]);
+
+                // Use a transaction to ensure data consistency
+                DB::beginTransaction();
+
+                Reservation::create([
+                    'name' => $this->name,
+                    'email' => $this->email,
+                    'mobile' => $this->mobile,
+                    'res_date' => $this->res_date,
+                    'res_time' => $this->res_time,
+                    'table_id' => $this->table_id,
+                    'guest_number' => $this->guest_number,
+                    'provider_id' => $this->provider_id,
+                    'reservation_status' => 'reserved',
+                    'user_id' => Auth::user()->id ?? null,
+                ]);
+
+                // Commit the transaction if all operations are successful
+                DB::commit();
+
+                // Reset component properties to clear the input values
+                $this->name = '';
+                $this->email = '';
+                $this->mobile = '';
+                $this->res_date = '';
+                $this->res_time = '';
+                $this->table_id = null;
+                $this->guest_number = '';
+                $this->provider_id = null;
+
+                notify()->success('Reservation successfully created.');
+            } catch (\Exception $e) {
+
+                // Handle the exception
+                DB::rollBack(); // Roll back the transaction if an exception occurs
+
+                // You can log the exception or show an error message to the user
+                // For now, let's log the exception and show a generic error message
+                Log::error('Error creating reservation: ' . $e->getMessage());
+
+                notify()->error('An error occurred while creating the reservation. Please try again later.');
+            }
+        }
+
+        public function thirdStep()
+        {
+            if ($this->step == 2) {
+                $this->validate([
+                    'table_id' => 'required',
+                ]);
+                $this->step = 3;
+            }
+        }
+
+        public function previousStep()
+        {
+            if ($this->step == 2) {
+                $this->step = 1;
+            }elseif ($this->step == 3){
+                $this->step = 2;
+            }
+        }
+
+        private function checkTableAvailability()
+        {
+            // Find a table that can accommodate the selected guest number and belongs to the selected provider
+            $tables = Table::where('guest_number', '>=', $this->guest_number)
+                ->where('provider_id', $this->provider_id)
+                ->get();
+
+            if ($tables->isEmpty()) {
+                return false;
+            }
+            $tableIds = $tables->pluck('id')->toArray();
 
 
-        return view('livewire.reservation-component')->layout('layouts.font-layout');
+            // Check if there are any reservations for the selected date and the found table
+            $existingReservation = Reservation::where('res_date', $this->res_date)
+                ->whereIn('table_id', $tables->pluck('id'))
+                ->where('reservation_status', 'reserved')
+                ->get();
+
+
+            if (!$existingReservation->isEmpty()) {
+                return false;
+            }
+
+            $reservedTableIds = $existingReservation->pluck('table_id')->toArray();
+
+            $availableTableIds = array_diff($tableIds, $reservedTableIds);
+
+            return $availableTableIds;
+        }
+
+
+
+        public function workHours()
+        {
+            $workingHours = WorkingHour::first();
+            $start_time = Carbon::parse($workingHours->start_time);
+            $end_time = Carbon::parse($workingHours->end_time);
+
+            $current_time = $start_time;
+            $time_intervals = [];
+
+            while ($current_time < $end_time) {
+                $time_intervals[] = $current_time->format('H:i:s');
+                $current_time->addHour();
+            }
+            return $time_intervals;
+        }
+
+        private function validateStepOne()
+        {
+            $this->validate([
+                'provider_id' => 'required',
+                'res_date' => ['required', 'date', $this->validateDateTimeNotBeforeNow($this->res_date, $this->res_time)],
+                'guest_number' => 'required|integer',
+                'res_time' => 'required'
+            ]);
+        }
+
+        private function validateDateTimeNotBeforeNow($date, $time)
+        {
+            // Combine the date and time inputs into a single datetime string
+            $dateTimeInput = $date . ' ' . $time;
+
+            // Parse the datetime input
+            $selectedDateTime = Carbon::parse($dateTimeInput);
+
+            // Get the current date and time
+            $currentDateTime = Carbon::now();
+
+            // Check if the selected datetime is before the current datetime
+            if ($selectedDateTime->lt($currentDateTime)) {
+                return 'after_or_equal:' . $currentDateTime->toDateTimeString();
+            }
+
+            return '';
+        }
+
+
+
+        public function render()
+        {
+            $resTimes = $this->workHours();
+            if ($this->step == 2) {
+                // Fetch available tables for the selected provider and reservation date
+            }
+
+            return view('livewire.reservation-component', ['resTimes' => $resTimes])->layout('layouts.font-layout');
+        }
     }
-}
--- resources/views/livewire/reservation-component.blade.php
+++ resources/views/livewire/reservation-component.blade.php
@@ -7,99 +7,176 @@
                         <div class="heading-section text-center">
                             @if ($step == 1)
                                 <span class="subheading">
-                                    Step 1
+                                    <h2 style="font-size: 15px; color: #0a53be">Step 1</h2>
                                 </span>
                                 <h2>
                                     Reservation
                                 </h2>
-                            @endif
-                            <form wire:submit.prevent="nextStep">
-                                @csrf
-                                <div class="row">
-                                    <div class="col-md-12 form-group">
-                                        <input type="text" class="form-control" wire:model="name" placeholder="Name">
-                                    </div>
-                                    @error('name')
-                                    <div class="text-sm text-red-400">{{ $message }}</div>
-                                    @enderror
 
-                                    <div class="col-md-12 form-group">
-                                        <input type="text" class="form-control" wire:model="email" placeholder="Email">
-                                    </div>
-                                    @error('email')
-                                    <div class="text-sm text-red-400">{{ $message }}</div>
-                                    @enderror
+                                <form wire:submit.prevent="nextStep">
+                                    @csrf
+                                    <div class="row">
 
-                                    <div class="col-md-12 form-group">
-                                        <input type="number" class="form-control" wire:model="mobile" placeholder="Phone">
-                                    </div>
-                                    @error('mobile')
-                                    <div class="text-sm text-red-400">{{ $message }}</div>
-                                    @enderror
-
-                                    <div class="col-md-12 form-group">
-                                        <select name="provider_id" class="form-control" id="selectProvider" wire:model="provider_id">
-                                            <optgroup label="Please Choose Restaurant">
+                                        <div class="col-md-12 form-group">
+                                            <label for="selectProvider" class="block text-sm font-medium text-gray-700">Select Restaurant</label>
+                                            <select name="provider_id" class="form-control" id="selectProvider" wire:model="provider_id">
+                                                <option value="">Please Choose Restaurant</option>
                                                 @if ($providers)
                                                     @foreach ($providers as $provider)
                                                         <option value="{{ $provider->id }}">{{ $provider->name }}</option>
                                                     @endforeach
                                                 @endif
-                                            </optgroup>
-                                        </select>
-                                    </div>
-
-                                    <div class="col-md-12 form-group">
-                                        <label for="res_date" class="block text-sm font-medium text-gray-700"> Reservation Date </label>
-                                        <div class="mt-1">
-                                            <input type="date" id="res_date" name="res_date" wire:model="res_date" class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                            </select>
+                                            @error('provider_id')
+                                            <div class="text-sm text-red-400">{{ $message }}</div>
+                                            @enderror
                                         </div>
-                                        <span class="text-xs">Please choose the time between 17:00-23:00.</span>
-                                        @error('res_date')
-                                        <div class="text-sm text-red-400">{{ $message }}</div>
-                                        @enderror
-                                    </div>
 
-                                    <div class="col-md-12 form-group">
-                                        <label for="guest_number" class="block text-sm font-medium text-gray-700"> Guest Number </label>
-                                        <div class="mt-1">
-                                            <input type="number" id="guest_number" name="guest_number" wire:model="guest_number" class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                        <div class="col-md-12 form-group">
+                                            <label for="res_date" class="block text-sm font-medium text-gray-700">Reservation Date</label>
+                                            <div class="mt-1">
+                                                <input type="date" id="res_date" name="res_date" wire:model="res_date" class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                            </div>
+                                            @error('res_date')
+                                            <div class="text-sm text-red-400">{{ $message }}</div>
+                                            @enderror
                                         </div>
-                                        @error('guest_number')
-                                        <div class="text-sm text-red-400">{{ $message }}</div>
-                                        @enderror
-                                    </div>
 
-                                    <div class="col-md-12 text-center">
-                                        <button class="btn btn-dark btn-shadow btn-lg" style="color: rgba(161,127,58,0.89)" type="submit" name="submit">Next</button>
-                                    </div>
-                                </div>
-                            </form>
-                            @if ($step == 2)
-                                <span class="subheading">
-                                    Step 2
-                                </span>
-                                <!-- Step 2: Display available tables -->
+                                        <div class="col-md-12 form-group">
+                                            <label for="selectWorkHour" class="block text-sm font-medium text-gray-700">Select Work Hours</label>
+                                            <select name="res_time" class="form-control" id="selectWorkHour" wire:model="res_time">
+                                                <option value="">Please Choose reservation time</option>
+                                                @if ($resTimes)
+                                                    @foreach ($resTimes as $resTime)
+                                                        <option value="{{ $resTime }}">{{ $resTime }}</option>
+                                                    @endforeach
+                                                @endif
+                                            </select>
+                                            <span style="color: #22c300;" class="text-xs">Please choose the time between 17:00-23:00.</span>
+                                            @error('res_time')
+                                            <div class="text-sm text-red-400">{{ $message }}</div>
+                                            @enderror
+                                        </div>
 
-                                @if ($availableTables->count() > 0)
-                                    <!-- Display the available tables based on the fetched data -->
-                                    <ul>
-                                        @foreach ($availableTables as $table)
-                                            <li>
-                                                <button wire:click="selectTable({{ $table->id }})">Select Table</button>
-                                                {{ $table->name }}
-                                            </li>
-                                        @endforeach
-                                    </ul>
-                                @else
-                                    <h3>No available tables found for the selected provider and date.</h3>
-                                    <button class="btn btn-dark btn-shadow btn-lg" wire:click="previousStep">Previous</button>
-                                @endif
+                                        <div class="col-md-12 form-group">
+                                            <label for="guest_number" class="block text-sm font-medium text-gray-700">Guest Number</label>
+                                            <div class="mt-1">
+                                                <input type="number" id="guest_number" name="guest_number" wire:model="guest_number" class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                            </div>
+                                            @error('guest_number')
+                                            <div class="text-sm text-red-400">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+
+                                        <div class="col-md-12 text-center">
+                                            <button class="btn btn-dark btn-shadow btn-lg" style="color: rgba(161,127,58,0.89)" type="submit" name="submit">Check Availability/Next</button>
+                                        </div>
+                                    </div>
+                                </form>
                             @endif
+                            @if ($step == 2)
+                                    <span class="subheading">
+                                    <h2 style="font-size: 15px; color: #0a53be">Step 2</h2>
+                                </span>
+                                    <h2>
+                                        Choose the table
+                                    </h2>
+
+                                    <form wire:submit.prevent="thirdStep">
+                                        @csrf
+                                        <div class="row">
+
+                                            <div class="col-md-12 form-group">
+                                                <label for="selectTable" class="block text-sm font-medium text-gray-700">Select Table</label>
+                                                <select name="table_id" class="form-control" id="selectTable" wire:model="table_id">
+                                                    <option value="">Please Choose a Table</option>
+                                                    @foreach ($availableTableIds as $table)
+                                                        <option value="{{ $table->id }}">Table {{ $table->name }}</option>
+                                                    @endforeach
+                                                </select>
+                                                @error('table_id')
+                                                <div class="text-sm text-red-400">{{ $message }}</div>
+                                                @enderror
+                                            </div>
+
+                                            <div class="col-md-12 form-group">
+                                                <div id="card-element"></div>
+
+
+                                                <!-- Used to display form errors. -->
+                                                <div id="card-errors" role="alert"></div>
+
+                                            </div>
+
+                                        </div>
+
+                                            <div class="col-md-12 text-center">
+                                            <button class="btn btn-dark btn-shadow btn-lg" wire:click="previousStep">Previous</button>
+
+                                            <button class="btn btn-dark btn-shadow btn-lg" style="color: rgba(161,127,58,0.89)" type="submit" name="submit">Next</button>
+                                        </div>
+                                    </form>
+                                @endif
+
+                                @if ($step == 3)
+                                    <span class="subheading">
+                                    <h2 style="font-size: 15px; color: #0a53be">Step 3</h2>
+                                </span>
+                                    <h2>
+                                        Write Your
+                                    </h2>
+
+                                    <form wire:submit.prevent="storeData">
+                                        @csrf
+                                        <div class="row">
+                                            <div class="col-md-12 form-group">
+                                                <input name="name" type="text" class="form-control" wire:model="name" placeholder="Name">
+                                            </div>
+                                            @error('name')
+                                            <div class="text-sm text-red-400">{{ $message }}</div>
+                                            @enderror
+
+                                            <div class="col-md-12 form-group">
+                                                <input type="text" name="email" class="form-control" wire:model="email" placeholder="Email">
+                                            </div>
+                                            @error('email')
+                                            <div class="text-sm text-red-400">{{ $message }}</div>
+                                            @enderror
+
+                                            <div class="col-md-12 form-group">
+                                                <input type="number" name="mobile" class="form-control" wire:model="mobile" placeholder="Phone">
+                                            </div>
+                                            @error('mobile')
+                                            <div class="text-sm text-red-400">{{ $message }}</div>
+                                            @enderror
+
+                                            <div class="col-md-12 text-center">
+                                                <button class="btn btn-dark btn-shadow btn-lg" wire:click="previousStep">Previous</button>
+
+                                                <button class="btn btn-dark btn-shadow btn-lg" style="color: rgba(161,127,58,0.89)" type="submit" name="submit">Book Now!</button>
+                                            </div>
+                                        </div>
+                                    </form>
+
+
+                                @endif
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
+    <x-notify::notify />
 </div>
+
+@push('scripts')
+    <script src="https://js.stripe.com/v3/"></script>
+    <script>
+        document.addEventListener("livewire:load", function() {
+            const stripe = Stripe('{{ config('services.stripe.key') }}');
+            const elements = stripe.elements();
+            const cardElement = elements.create('card');
+            cardElement.mount('#card-element');
+        });
+    </script>
+@endpush
