410b3b1fe14f28a57bc1f2dbfbdf82ae85ba0b64
Aya Hosny <96696906+ayahosnii@users.noreply.github.com>
2023-09-24 11:48:58 +0300

    update

--- resources/views/layouts/font-layout.blade.php
+++ resources/views/layouts/font-layout.blade.php
@@ -22,6 +22,72 @@
     <link rel="stylesheet" href="{{asset('assets/css/slicknav.min.css')}}" type="text/css">
     <link rel="stylesheet" type="text/css" href="{{asset('assets/fonts/flag-icon-css/css/flag-icon.css')}}">
     <link rel="stylesheet" href="{{asset('assets/css/style.css')}}" type="text/css">
+    <style>
+        .btn-whatsapp-pulse {
+            background: #25d366;
+            color: white;
+            position: fixed;
+            bottom: 20px;
+            right: 20px;
+            font-size: 40px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 0;
+            height: 0;
+            padding: 35px;
+            text-decoration: none;
+            border-radius: 50%;
+            animation-name: pulse;
+            animation-duration: 1.5s;
+            animation-timing-function: ease-out;
+            animation-iteration-count: infinite;
+            z-index: 99999;
+        }
+
+        @keyframes pulse {
+            0% {
+                box-shadow: 0 0 0 0 rgba(37, 211, 102, 0.5);
+            }
+            80% {
+                box-shadow: 0 0 0 14px rgba(37, 211, 102, 0);
+            }
+        }
+
+        .btn-whatsapp-pulse-border {
+            bottom: 120px;
+            right: 20px;
+            animation-play-state: paused;
+        }
+
+        .btn-whatsapp-pulse-border::before {
+            content: "";
+            position: absolute;
+            border-radius: 50%;
+            padding: 25px;
+            border: 5px solid #25d366;
+            opacity: 0.75;
+            animation-name: pulse-border;
+            animation-duration: 1.5s;
+            animation-timing-function: ease-out;
+            animation-iteration-count: infinite;
+        }
+
+        @keyframes pulse-border {
+            0% {
+                padding: 25px;
+                opacity: 0.75;
+            }
+            75% {
+                padding: 50px;
+                opacity: 0;
+            }
+            100% {
+                opacity: 0;
+            }
+        }
+
+    </style>
     @stack('styles')
     @livewireStyles
     @notifyCss
@@ -45,7 +111,14 @@
 
 <!-- Header Section End -->
 {{$slot}}
+
 @include('includes.footer')
+
+
+<a href="https://wa.me/201006215138" class="btn-whatsapp-pulse btn-whatsapp-pulse-border">
+    <i class="fa fa-whatsapp"></i>
+</a>
+
 <!-- Js Plugins -->
 {{--<script src="{{asset('assets/js/fontawesome/all.min.js')}}"></script>--}}
 <script src="{{asset('assets/js/jquery-3.6.0.min.js')}}"></script>
